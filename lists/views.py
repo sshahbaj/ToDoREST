3d903e74a661c6340bea3ac2051bbdbee214d063
sshahbaj <shahbajsayyad.s@gmail.com>
2018-08-20 19:03:43 +0530

    Added custom error message and POST will expect a dict.

--- lists/views.py
+++ lists/views.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 
 # class IndexViewSet(viewsets.ModelViewSet):
@@ -15,7 +16,10 @@ from django.http import JsonResponse
 def todo_list(request, pk=None):
     if request.method == 'GET':
         if pk:
-            todo = ToDoItem.objects.get(pk=pk).__dict__
+            try:
+                todo = ToDoItem.objects.get(pk=pk).__dict__
+            except ObjectDoesNotExist:
+                return Response({'auto_increment_id': pk, 'error': 'Task with this id does not exists'}, status.HTTP_404_NOT_FOUND)
             todo.pop('_state', None)
             return JsonResponse(todo, safe=False)
         else:
@@ -24,21 +28,23 @@ def todo_list(request, pk=None):
 
     elif request.method == 'POST':
         try:
-            todo = request.data.dict()
-            if todo.get('date', None):
+            todo = request.data
+            if todo.get('date', None) and todo['date'] != '':
                 todo['date'] = datetime.strptime(todo['date'], '%d/%m/%Y').date()
+            else:
+                todo['date'] = None
             test = ToDoItem(**todo)
             test.save()
             test.__dict__.pop('_state', None)
             return JsonResponse(test.__dict__)
         except ValueError:
-            return Response({"date":"Date format should be in DD-MM-YYYY"}, status.HTTP_400_BAD_REQUEST)
+            return Response({"date":"Date format should be in DD/MM/YYYY"}, status.HTTP_400_BAD_REQUEST)
 
     elif pk:
         try:
             todo = ToDoItem.objects.get(pk=pk)
         except ToDoItem.DoesNotExist:
-            return Response(status.HTTP_404_NOT_FOUND)
+            return Response({'auto_increment_id': pk, 'error': 'Task with this id does not exists'}, status.HTTP_404_NOT_FOUND)
 
         if request.method == 'DELETE':
             todo.delete()
@@ -51,6 +57,10 @@ def todo_list(request, pk=None):
                 todo.save()
                 return Response(status.HTTP_204_NO_CONTENT)
             return Response(status.HTTP_400_BAD_REQUEST)
+
+
+
+
 
 
 
