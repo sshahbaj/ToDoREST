a185097628f7c89d1a314cac769e99ee7127047d
sshahbaj <shahbajsayyad.s@gmail.com>
2018-08-21 22:42:52 +0530

    Altered model field.

--- lists/models.py
+++ lists/models.py
@@ -4,9 +4,23 @@ import datetime
 
 class ToDoItem(models.Model):
     subject = models.CharField(max_length=100)
-    date = models.DateField(default=datetime.date.today)
+    date = models.DateField(null=True)
     auto_increment_id = models.AutoField(primary_key=True, default=None)
     done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.subject
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    pub_date = models.DateField()
