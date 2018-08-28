0ab8bf3127b76d7c194fd371d778502d104041ef
sshahbaj <shahbajsayyad.s@gmail.com>
2018-08-28 19:16:45 +0530

    Added User foreign key to ToDoItem.

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 import datetime
 
 
 class ToDoItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, blank=False)
     subject = models.CharField(max_length=100)
     date = models.DateField(null=True)
     auto_increment_id = models.AutoField(primary_key=True, default=None)
