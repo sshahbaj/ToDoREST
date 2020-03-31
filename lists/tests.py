6e162ef42ad3bad0dd705233f91c2ae510b19896
Shahbaj Sayyad <shahbajsayyad.s@gmail.com>
2020-03-31 17:56:35 +0530

    Update tests

--- lists/tests.py
+++ lists/tests.py
@@ -2,5 +2,5 @@ from django.test import TestCase
 
 
 class TestFoobar(TestCase):
-    def test_should_pass(self):
-        self.assertTrue(True)
+    def test_should_fail(self):
+        self.assertTrue(False)
